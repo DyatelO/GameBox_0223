0ce823cbf80cf9a1eaeccd9c45b71239a63e53d8
DyatelO <ishitory89@yandex.ru>
2023-02-17 16:07:51 +0300

    Секира работает.

--- VampireSurvival-Like/Assets/Scripts/Weapon/ThrowingAxeWeapon.cs
+++ VampireSurvival-Like/Assets/Scripts/Weapon/ThrowingAxeWeapon.cs
@@ -3,11 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowingAxeWeapon : MonoBehaviour
+public class ThrowingAxeWeapon : WeaponBase
 {
-    [SerializeField] private float timeToAttack = 1.5f;
-    private float timer;
-
     private PlayerMove _playerMove;
 
     [SerializeField] private GameObject axePrefab;
@@ -17,32 +14,11 @@ public class ThrowingAxeWeapon : MonoBehaviour
         _playerMove = FindObjectOfType<PlayerMove>();
     }
 
-    private void Update()
-    {
-        if (timer < timeToAttack)
-        {
-            timer += Time.deltaTime;
-            return;
-        }
-
-        timer = 0;
-        SpawnAxe();
-    }
-
-    private void FixedUpdate()
-    {
-        //if (timer == 0)
-        //{
-        //    SpawnAxe();
-        //}
-    }
-
-    private void SpawnAxe()
+    public override void Attack()
     {
         GameObject thrownAxe = Instantiate(axePrefab);
         thrownAxe.transform.position = transform.position;
         //thrownAxe.transform.localEulerAngles = Vector3.forward * -45;
-        thrownAxe.GetComponent<ThrownAxeProjectile>().SetDirection(_playerMove.LastHorizontalVectorLength , Vector2.up.y + 10);
-            //_playerMove.LastHorizontalVectorLength, 0.125f);
+        thrownAxe.GetComponent<ThrownAxeProjectile>().SetDirection(_playerMove.LastHorizontalVectorLength, Vector2.up.y + 10);
     }
 }
