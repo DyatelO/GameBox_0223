3c9fa6311d5b64f4e5b23d0660faec1fa0b5b6f9
DyatelO <ishitory89@yandex.ru>
2023-02-17 18:57:40 +0300

    Иконки на апгрейды первый этап.

--- VampireSurvival-Like/Assets/Scripts/Level/Level.cs
+++ VampireSurvival-Like/Assets/Scripts/Level/Level.cs
@@ -8,6 +8,9 @@ public class Level : MonoBehaviour
     private int experience = 0;
     [SerializeField] private ExperienceProgress experienceProgress;
     [SerializeField] private UpgradePanelController upgradePanelController;
+
+    [SerializeField] private List<UpgradeData> upgradeDatasList;
+
     public int LevelUp => level * 1000;
 
     private void Start()
@@ -33,9 +36,27 @@ public class Level : MonoBehaviour
 
     private void NextLevel()
     {
-        upgradePanelController.OpeneMenu();
+        upgradePanelController.OpeneMenu(GetUpgradesList(4));
         experience -= LevelUp;
         level++;
         experienceProgress.SetLevelText(level);
+    }
+
+    public List<UpgradeData> GetUpgradesList(int count)
+    {
+        List<UpgradeData> upgradeList = new List<UpgradeData>();
+
+        if(count >  upgradeDatasList.Count)
+        {
+            count = upgradeDatasList.Count;
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+
+            upgradeList.Add(upgradeDatasList[Random.Range(0, upgradeDatasList.Count)]);
+        }
+
+        return upgradeList;
     }
 }
--- VampireSurvival-Like/Assets/Scripts/Menu/UpgradePanelController.cs
+++ VampireSurvival-Like/Assets/Scripts/Menu/UpgradePanelController.cs
@@ -7,6 +7,8 @@ public class UpgradePanelController : MonoBehaviour
     [SerializeField] private GameObject levelUpPanel;
     private PauseController pauseController;
 
+    [SerializeField] private List<UpgradeButton> upgradeButtons;
+
     private void Awake()
     {
         pauseController = GetComponent<PauseController>();
@@ -17,9 +19,14 @@ public class UpgradePanelController : MonoBehaviour
         levelUpPanel.SetActive(false);
         pauseController.ContinueGame();
     }
-    public void OpeneMenu()
+    public void OpeneMenu(List<UpgradeData> upgradeDatas)
     {
         pauseController.PauseGame();
         levelUpPanel.SetActive(true);
+
+        for (int i = 0; i < upgradeDatas.Count; i++)
+        {
+            upgradeButtons[i].Set(upgradeDatas[i]); 
+        }
     }
 }
